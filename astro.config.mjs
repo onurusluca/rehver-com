0781f56943a17e31e93ba88a58b810e83381eb47
onurusluca <uslucaonur@gmail.com>
2025-07-11 13:24:32 +0900

    wip 4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
     site: 'https://rehver.com',
     output: 'static',
-    adapter: vercel(),
+    // adapter: vercel(),
     vite: {
         resolve: {
             alias: {
